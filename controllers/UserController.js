da0417647976dec6b519f1076057561a1a2c2f72
luizfonsecaa <33426620+luizfonsecaa@users.noreply.github.com>
2018-09-05 10:10:02 -0300

    Atualização

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -5,7 +5,7 @@ class UserController{
         this.tableEl = document.getElementById(tableId);
         this.onSubmit();
     }
-    
+
     //metodo disparado quando o botao for clicado
     onSubmit(){
         this.formEl.addEventListener("submit", event => {
@@ -13,18 +13,18 @@ class UserController{
             let values = this.getValue();
 
             this.getPhoto().then(
-                (content) => { 
+                (content) => {
                     values.photo = content;
                     this.addLine(values);
-                }, 
+                },
                 (e) => {
                     console.error(e);
                 }
             );
         });
     }
-  
-    // percorrendo todos os elementos de user 
+
+    // percorrendo todos os elementos de user
     getValue(){
         let user = {};
         [...this.formEl.elements].forEach((field, index)=>{
@@ -34,7 +34,7 @@ class UserController{
                 user[field.name] = field.value;
             }
         });
-        return new User(user.name, user.gender, user.birth, user.country, 
+        return new User(user.name, user.gender, user.birth, user.country,
                                      user.email, user.password, user.photo, user.admin);
     }
 
@@ -55,7 +55,7 @@ class UserController{
                 reject(e);
             };
             if(file){
-                fileReader.readAsDataURL(file);    
+                fileReader.readAsDataURL(file);
             } else{
                 resolve('dist/img/boxed-bg.jpg');
             }
@@ -70,12 +70,12 @@ class UserController{
                 <td>${dataUser.name}</td>
                 <td>${dataUser.email}</td>
                 <td>${dataUser.admin}</td>
-                <td>${dataUser.birth}</td>
+                <td>${Util.dateFormat(dataUser.register)}</td>
                 <td>
                     <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
                     <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
                 </td>
             </tr>
         `;
-    } 
+    }
 }
